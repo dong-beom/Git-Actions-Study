733a21aaefb8d4ea2b1a3cb8af0446bc4d457043
dongbeomkim <dongbeom.kim@bespinglobal.com>
2023-05-30 05:02:24 +0900

    spotbug 추가

--- src/main/java/com/NullPointerExceptionExample.java
+++
@@ -1,9 +0,0 @@
-package com;
-
-public class NullPointerExceptionExample {
-    public static void main(String[] args) {
-        String str = null;
-        int length = str.length(); // NullPointerException 발생
-        System.out.println("문자열 길이: " + length);
-    }
-}
---
+++ src/main/java/com/SQLInjectionExample.java
@@ -0,0 +1,14 @@
+package com;
+
+public class SQLInjectionExample {
+    public static void main(String[] args) {
+        String username = getUserInput();
+        String query = "SELECT * FROM users WHERE username = '" + username + "'";
+        // query 실행 로직
+    }
+
+    private static String getUserInput() {
+        // 사용자로부터 입력 받는 로직
+        return "admin' --";
+    }
+}
