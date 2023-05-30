4362953b3e70e23870028a7d88776a629a00fb84
dongbeomkim <dongbeom.kim@bespinglobal.com>
2023-05-31 03:30:32 +0900

    second

--- src/main/java/com/SQLInjectionExample.java
+++ src/main/java/com/SQLInjectionExample.java
@@ -15,4 +15,9 @@ public class SQLInjectionExample {
     private static String firstMethod() {
         return "1";
     }
+
+    private static String secondMethod() {
+        // 사용자로부터 입력 받는 로직
+        return "2";
+    }
 }
